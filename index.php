031bd766db0156bc5ad22783ed6e24d2d16e6202
Raffael190605 <152363550+Raffael190605@users.noreply.github.com>
2024-03-18 20:21:44 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+include_once('top.php');
+include_once('menu.php');
+?>
+<style>
+    h1{
+        font-family: poppins, sans-serif;
+        font-weight: 500;
+        text-decoration: underline;
+    }
+</style>
+<main>
+    <div class="container-fluid px-4">
+        <h1 class="mt-4">Dashboard-Admin</h1>
+
+        <h5><i>Selamat Datang di halaman Admin</i></h5>
+    </div>
+</main>
+
+<?php
+include_once('bottom.php');
+?>
